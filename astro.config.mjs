1133ae48e9dcb119a92dbd744ed91ffd50fe4671
Camilo Acuña Y <74308419+camiloacunay@users.noreply.github.com>
2026-06-16 13:39:59 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://camiloacunay.github.io',
+  base: '/impulsolocal/',
   integrations: [react()],
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
